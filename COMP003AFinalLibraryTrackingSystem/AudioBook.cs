17366f992cd4d8dbe40a9f41d0e7463c3ece3f31
Angel Gomez <angelgomez5975@my.whccd.edu>
2025-03-02 14:59:00 -0800

    Added private fields. Added properties with implementation and validation. Added method override. -Audio Book derived class.

--- COMP003AFinalLibraryTrackingSystem/AudioBook.cs
+++ COMP003AFinalLibraryTrackingSystem/AudioBook.cs
@@ -5,5 +5,49 @@ namespace COMP003AFinalLibraryTrackingSystem;
 /// </summary>
 internal class AudioBook : Book
 {
+    // fields (private)
+    private string _duration; 
+    private string _fileFormat; 
+    private double _fileSize;
     
+    // Properties (public) with implementation and validation
+    public string Duration
+    {
+        // implementation
+        get { return _duration; }
+        set // validation ensuring that the Duration's value is not empty
+        {
+            if (string.IsNullOrWhiteSpace(value))
+            {
+                throw new ArgumentException("Duration cannot be empty. ");
+            }
+        }
+    }
+    
+    public string FileFormat
+    {
+        // implementation
+        get { return _fileFormat; }
+        set // validation ensuring that the File Format's value is not empty
+        {
+            if (string.IsNullOrWhiteSpace(value))
+            {
+                throw new ArgumentException("File Format cannot be empty. ");
+            }
+            _fileFormat = value;
+        }
+    }
+
+    public double FileSize
+    {
+        // implementation
+        get { return _fileSize; }
+        set { _fileSize = value; }
+    }
+    
+    // Empty override GetDetails() from the Book abstract class
+    public override void GetDetails()
+    {
+        ;
+    }
 }
