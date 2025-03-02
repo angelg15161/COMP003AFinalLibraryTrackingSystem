e076f4a606ebcdccc02d1fe7d9bda3f6f812349e
Angel Gomez <angelgomez5975@my.whccd.edu>
2025-03-02 14:58:14 -0800

    Added private fields. Added properties with implementation and validation. Added method override. -Comic Book derived class.

--- COMP003AFinalLibraryTrackingSystem/ComicBook.cs
+++ COMP003AFinalLibraryTrackingSystem/ComicBook.cs
@@ -5,5 +5,27 @@ namespace COMP003AFinalLibraryTrackingSystem;
 /// </summary>
 internal class ComicBook : Book
 {
+    // fields (private)
+    private string _series;
     
+    // Properties (public) with implementation and validation
+    public string Series
+    {
+        // implementation
+        get { return _series; }
+        set // validation ensuring that the Series' value is not empty
+        {
+            if (string.IsNullOrWhiteSpace(value))
+            {
+                throw new ArgumentException("Series cannot be empty. ");
+            }
+            _series = value;
+        }
+    }
+    
+    // Override GetDetails() from the Book abstract class
+    public override void GetDetails()
+    {
+        Console.WriteLine("Comic Books include a Title, Author, ISBN, Publisher, Published year, and Series. ");
+    }
 }
