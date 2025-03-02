cf6146893f1de0e51e965e247f5366c99742a9e4
Angel Gomez <angelgomez5975@my.whccd.edu>
2025-03-02 13:34:51 -0800

    Created a Physical Book derived class which inherits the Book abstract class.

---
+++ COMP003AFinalLibraryTrackingSystem/PhysicalBook.cs
@@ -0,0 +1,9 @@
+namespace COMP003AFinalLibraryTrackingSystem;
+
+/// <summary>
+/// Physical Book derived class which is a type of Book. 
+/// </summary>
+internal class PhysicalBook : Book
+{
+    
+}
