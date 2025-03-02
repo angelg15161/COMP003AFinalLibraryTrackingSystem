a036a5b88e8a6f09fa1a3ad8dbb477cb663433fb
Angel Gomez <angelgomez5975@my.whccd.edu>
2025-03-02 13:59:24 -0800

    Added private fields. Added properties with implementation and validation. -Physical Book derived class.

--- COMP003AFinalLibraryTrackingSystem/PhysicalBook.cs
+++ COMP003AFinalLibraryTrackingSystem/PhysicalBook.cs
@@ -5,5 +5,29 @@ namespace COMP003AFinalLibraryTrackingSystem;
 /// </summary>
 internal class PhysicalBook : Book
 {
-    
+    // fields (private)
+    private int _pages;
+    private string _genre; 
+
+    // Properties (public) with implementation and validation 
+    public int Pages
+    {
+        // implementation
+        get { return _pages; }
+        set { _pages = value; }
+    }
+
+    public string Genre
+    {
+        // implementation
+        get { return _genre; }
+        set // validation ensuring that the Genre's value is not empty
+        {
+            if (string.IsNullOrEmpty(value))
+            {
+                throw new ArgumentException("Genre cannot be empty. ");
+            }
+            _genre = value;
+        }
+    }
 }
