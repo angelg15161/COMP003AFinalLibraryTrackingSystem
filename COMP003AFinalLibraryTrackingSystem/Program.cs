9be62266bb10c469e16c502dedc15b1cdac41746
Angel Gomez <angelgomez5975@my.whccd.edu>
2025-03-02 11:38:45 -0800

    Librarian password to gain access to the program.

--- COMP003AFinalLibraryTrackingSystem/Program.cs
+++ COMP003AFinalLibraryTrackingSystem/Program.cs
@@ -1,9 +1,36 @@
-﻿namespace COMP003AFinalLibraryTrackingSystem;
+﻿/*
+    Author: Angel Gomez
+    Course: COMP-003A
+    Faculty: Jonathan Cruz
+    Purpose: To serve as a library’s book tracking system.
+*/
+namespace COMP003AFinalLibraryTrackingSystem;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        // Welcome message 
+        Console.WriteLine("Welcome to the library's book tracker! ");
+        bool pass = true;
+
+        while (pass)
+        {
+            string libraryPassword = "Password123"; // Preset library password 
+            
+            // Prompt the user to enter librarian password 
+            Console.Write("\nPlease enter the Librarian's Password: ");
+            string password = Console.ReadLine().Trim();
+
+            if (password == libraryPassword)
+            {
+                Console.WriteLine("Password accepted! ");
+                pass = false;
+            }
+            else 
+            {
+                Console.WriteLine("Incorrect Librarian Password, Try Again. ");
+            }
+        }
     }
 }
