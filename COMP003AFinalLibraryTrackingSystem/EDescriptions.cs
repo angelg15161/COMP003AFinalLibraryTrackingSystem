e682d73c111dfa5126b1afdb429a00f01b94703d
Angel Gomez <angelgomez5975@my.whccd.edu>
2025-03-02 15:07:39 -0800

    Created a class for method overloading E-Book and Audio Book Descriptions.

---
+++ COMP003AFinalLibraryTrackingSystem/EDescriptions.cs
@@ -0,0 +1,9 @@
+namespace COMP003AFinalLibraryTrackingSystem;
+
+/// <summary>
+/// Method overloading class for E-Book and Audio Book descriptions. 
+/// </summary>
+internal class EDescriptions
+{
+    
+}
