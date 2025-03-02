cb6fb8eff5b24e7896cbbeb68e7bffdf311dd311
Angel Gomez <angelgomez5975@my.whccd.edu>
2025-03-02 14:58:48 -0800

    Added private fields. Added properties with implementation and validation. Added method override. -E-Book derived class.

--- COMP003AFinalLibraryTrackingSystem/EBook.cs
+++ COMP003AFinalLibraryTrackingSystem/EBook.cs
@@ -5,5 +5,35 @@ namespace COMP003AFinalLibraryTrackingSystem;
 /// </summary>
 internal class EBook : Book 
 {
+    // fields (private)
+    private string _fileFormat; 
+    private double _fileSize;
     
+    // Properties (public) with implementation and validation
+    public string FileFormat
+    {
+        // implementation
+        get { return _fileFormat; }
+        set // validation ensuring that the File Format's value is not empty
+        {
+            if (string.IsNullOrWhiteSpace(value))
+            {
+                throw new ArgumentException("File Format cannot be empty. ");
+            }
+            _fileFormat = value;
+        }
+    }
+
+    public double FileSize
+    {
+        // implementation
+        get { return _fileSize; }
+        set { _fileSize = value; }
+    }
+    
+    // Empty override GetDetails() from the Book abstract class
+    public override void GetDetails()
+    {
+        ;
+    }
 }
