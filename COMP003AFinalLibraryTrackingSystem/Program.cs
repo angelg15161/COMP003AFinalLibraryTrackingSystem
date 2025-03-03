4ab18635191242e0a97487025b507a5436673a2f
Angel Gomez <angelgomez5975@my.whccd.edu>
2025-03-02 22:47:21 -0800

    Added Main Menu for Selecting options 1-6 which call for methods to add books, edit books, remove books, view books, see book type descriptions, and exit the program.

--- COMP003AFinalLibraryTrackingSystem/Program.cs
+++ COMP003AFinalLibraryTrackingSystem/Program.cs
@@ -13,7 +13,7 @@ class Program
         // Welcome message 
         Console.WriteLine("Welcome to the library's book tracker! ");
         bool pass = true;
-
+        
         while (pass)
         {
             string libraryPassword = "Password123"; // Preset library password 
@@ -30,6 +30,64 @@ class Program
             else 
             {
                 Console.WriteLine("Incorrect Librarian Password, Try Again. ");
+            }
+        }
+
+        bool exit = true; 
+        while (exit) // Menu loop
+        {
+            Console.WriteLine("\n\nMain Menu: \n1. Add a Book \n2. Edit a Book \n3. Remove a Book \n4. View Books");
+            Console.WriteLine("5. Book Type Descriptions \n6. Exit");
+            Console.Write("Enter your choice (1-6): ");
+            
+            try
+            {
+                int choice = int.Parse(Console.ReadLine());
+                if (choice == 1)
+                {
+                    BookInfo.AddBook();
+                }
+                else if (choice == 2)
+                {
+                    BookInfo.EditBook();
+                }
+                else if (choice == 3)
+                {
+                    BookInfo.RemoveBook();
+                }
+                else if (choice == 4)
+                {
+                    BookInfo.ViewBooks();
+                }
+                else if (choice == 5)
+                {
+                    Book PhysicalBook = new PhysicalBook();
+                    Book ComicBook = new ComicBook();
+                    
+                    PhysicalBook.GetDetails();
+                    ComicBook.GetDetails();
+                    
+                    string description = " include a Title, Author, ISBN, Publisher, Published year, File Format, File Size";
+                    string description2 = ", Duration ";
+                    string bookType1 = "E-Books";
+                    string bookType2 = "Audio Books";
+                    
+                    EDescriptions.EDescription(bookType1, description);
+                    EDescriptions.EDescription(bookType2, description, description2);
+                }
+                else if (choice == 6)
+                {
+                    Console.WriteLine("\nGoodbye!");
+                    exit = false;
+                }
+                else // Prompts user to select number 1-4 if another number was selected
+                {
+                    Console.WriteLine("Invalid input. Please select a number 1-4."); 
+                }
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Invalid input. Try again."); 
             }
         }
     }
